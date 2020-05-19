fac1641b52323f32dadbc88e622cf0a2f34d44d9
Maxim Shoustin <maxim@appsflyer.com>
2020-05-19 14:00:29 +0300

    Revert "5.3.1"

--- Plugins/iOS/AppsFlyerAppController.mm
+++ Plugins/iOS/AppsFlyerAppController.mm
@@ -58,7 +58,6 @@
 }
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
-    [super application:application continueUserActivity:userActivity restorationHandler:restorationHandler];
     [[AppsFlyerTracker sharedTracker] continueUserActivity:userActivity restorationHandler:restorationHandler];
     return YES;
 }
