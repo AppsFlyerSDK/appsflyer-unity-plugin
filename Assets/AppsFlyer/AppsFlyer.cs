54259b9c4ea3ebd639a3494622630053a4c1e329
Margot <margot.guetta@appsflyer.com>
2022-04-12 09:30:04 +0300

    update to 6.5.4

--- Assets/AppsFlyer/AppsFlyer.cs
+++ Assets/AppsFlyer/AppsFlyer.cs
@@ -7,7 +7,7 @@ namespace AppsFlyerSDK
     public class AppsFlyer : MonoBehaviour
     {
 
-        public static readonly string kAppsFlyerPluginVersion = "6.5.3";
+        public static readonly string kAppsFlyerPluginVersion = "6.5.4";
         public static string CallBackObjectName = null;
         private static EventHandler onRequestResponse;
         private static EventHandler onInAppResponse;
