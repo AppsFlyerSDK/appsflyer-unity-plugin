4cb8bbefcb4a703b8016e1fc45ffb67c78ff5d14
af-margot <48682256+af-margot@users.noreply.github.com>
2022-07-21 09:36:26 -0700

    Merge pull request #160 from AppsFlyerSDK/releases/6.x.x/6.6.x/6.6.1
    release 6.6.1

---
+++ IAppsFlyerIOSBridge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace AppsFlyerSDK
+{
+
+	public interface IAppsFlyerIOSBridge : IAppsFlyerNativeBridge
+	{
+        void setDisableCollectAppleAdSupport(bool disable);
+		void setShouldCollectDeviceName(bool shouldCollectDeviceName);
+		void setDisableCollectIAd(bool disableCollectIAd);
+		void setUseReceiptValidationSandbox(bool useReceiptValidationSandbox);
+		void setUseUninstallSandbox(bool useUninstallSandbox);
+		void validateAndSendInAppPurchase(string productIdentifier, string price, string currency, string tranactionId, Dictionary<string, string> additionalParameters, MonoBehaviour gameObject);
+		void registerUninstall(byte[] deviceToken);
+		void handleOpenUrl(string url, string sourceApplication, string annotation);
+		void waitForATTUserAuthorizationWithTimeoutInterval(int timeoutInterval);
+		void setCurrentDeviceLanguage(string language);
+		void disableSKAdNetwork(bool isDisabled);
+
+	}
+}
