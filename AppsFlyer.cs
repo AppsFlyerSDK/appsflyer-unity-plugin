6fbf3433de636d44529d3a6dd626a02fc386d348
Margot <margot.guetta@appsflyer.com>
2022-07-26 09:41:31 -1000

    update

--- AppsFlyer.cs
+++ AppsFlyer.cs
@@ -7,7 +7,7 @@ namespace AppsFlyerSDK
     public class AppsFlyer : MonoBehaviour
     {
 
-        public static readonly string kAppsFlyerPluginVersion = "1.1.2";
+        public static readonly string kAppsFlyerPluginVersion = "1.1.6";
         public static string CallBackObjectName = null;
         private static EventHandler onRequestResponse;
         private static EventHandler onInAppResponse;
