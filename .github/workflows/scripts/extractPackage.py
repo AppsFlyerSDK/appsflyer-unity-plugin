b38ec369613b61a176c318165cd42805b8fce7fd
af-margot <48682256+af-margot@users.noreply.github.com>
2022-10-09 11:37:50 +0300

    Update extractPackage.py

--- .github/workflows/scripts/extractPackage.py
+++ .github/workflows/scripts/extractPackage.py
@@ -11,8 +11,8 @@ class checkPackage:
     def __init__(self, pathToPackage):
         self.pathToPackage = pathToPackage
 
-    def extractPackage(self):
-        extractPackage(self.pathToPackage, outputPath="./packageUnity")
+    def extractPackage(self, pathToOuptut):
+        extractPackage(self.pathToPackage, outputPath=pathToOuptut)
 
     def hasCommentedMethods(self, file):
         textfile = open(file, 'r')
@@ -28,41 +28,30 @@ def main():
     package = checkPackage("appsflyer-unity-plugin-6.6.0.unitypackage")
     strict_package = checkPackage("strict-mode-sdk/appsflyer-unity-plugin-strict-mode-6.6.0.unitypackage")
 
-    #testing regular package
+    #testing integreity of files
     print("Testing AppsFlyeriOSWrapper.mm in unity package")
-    package.extractPackage()
-    hasComments = package.hasCommentedMethods("packageUnity/Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm")
-    print("The file in the unity package has comments : ", hasComments)
-
+    package.extractPackage("./packageUnity")
+    
+    path_of_the_strict_directory= 'strictPackageUnity/Assets/AppsFlyer/Plugins/iOS/'
     path_of_the_directory= 'packageUnity/Assets/AppsFlyer/Plugins/iOS/'
     path_of_repo = "Assets/AppsFlyer/Plugins/iOS/"
     for filename in os.listdir(path_of_the_directory):
         f1 = os.path.join(path_of_the_directory,filename)
-        if os.path.isfile(f1):
-            print(f1)
-            f2 = os.path.join(path_of_repo, filename)
-            if os.path.isfile(f2):
-                if getHash(f1) != getHash(f2):
-                    print("the file ", filename, "is not the same")
-
-    #testing strict mode package
-    print("Testing AppsFlyeriOSWrapper.mm in unity strict package")
-    strict_package.extractPackage()
-    strict_package.extractPackage()
-    hasComments = strict_package.hasCommentedMethods("packageUnity/Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm")
-    print("The file in the unity strict package has comments : ", hasComments)
-
-    # path_of_the_directory= 'packageUnity/Assets/AppsFlyer/Plugins/iOS/'
-    # path_of_repo = "Assets/AppsFlyer/Plugins/iOS/"
-    for filename in os.listdir(path_of_the_directory):
-        f1 = os.path.join(path_of_the_directory,filename)
-        if os.path.isfile(f1):
-            print(f1)
-            f2 = os.path.join(path_of_repo, filename)
-            if os.path.isfile(f2):
+        f2 = os.path.join(path_of_repo, filename)
+        f3 = os.path.join(path_of_the_strict_directory, filename)
+        if os.path.isfile(f1) and os.path.isfile(f2) and os.path.isfile(f3):
+            print(filename)
+            if filename == "AppsFlyeriOSWrapper.mm":
                 if getHash(f1) != getHash(f2):
                     print("the file ", filename, "is not the same")
                     sys.exit(5)
+            else:
+                if getHash(f1) != getHash(f2) or getHash(f3) != getHash(f2):
+                    print("the file ", filename, "is not the same")
+                    sys.exit(5)
+
+            
+               
 
     
 def getHash(filePath):
