f3a172c76a6b6fca1e0582b3a5878a53e5451a08
Margot <margot.guetta@appsflyer.com>
2022-10-09 11:15:55 +0300

    update python

--- .github/workflows/scripts/extractPackage.py
+++ .github/workflows/scripts/extractPackage.py
@@ -1,6 +1,8 @@
 from importlib.resources import path
 from nis import match
 import re
+import hashlib
+import os
 from unitypackage_extractor.extractor import extractPackage
 
 class checkPackage:
@@ -19,6 +21,8 @@ class checkPackage:
         return len(matches) > 0
 
 
+
+
 def main():
     package = checkPackage("appsflyer-unity-plugin-6.6.0.unitypackage")
     strict_package = checkPackage("strict-mode-sdk/appsflyer-unity-plugin-strict-mode-6.6.0.unitypackage")
@@ -29,14 +33,42 @@ def main():
     hasComments = package.hasCommentedMethods("packageUnity/Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm")
     print("The file in the unity package has comments : ", hasComments)
 
+    path_of_the_directory= 'packageUnity/Assets/AppsFlyer/Plugins/iOS/'
+    path_of_repo = "Assets/AppsFlyer/Plugins/iOS/"
+    for filename in os.listdir(path_of_the_directory):
+        f1 = os.path.join(path_of_the_directory,filename)
+        if os.path.isfile(f1):
+            print(f1)
+            f2 = os.path.join(path_of_repo, filename)
+            if os.path.isfile(f2):
+                if getHash(f1) != getHash(f2):
+                    print("the file ", filename, "is not the same")
+
     #testing strict mode package
     print("Testing AppsFlyeriOSWrapper.mm in unity strict package")
     strict_package.extractPackage()
     strict_package.extractPackage()
     hasComments = strict_package.hasCommentedMethods("packageUnity/Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm")
     print("The file in the unity strict package has comments : ", hasComments)
-    
 
+    # path_of_the_directory= 'packageUnity/Assets/AppsFlyer/Plugins/iOS/'
+    # path_of_repo = "Assets/AppsFlyer/Plugins/iOS/"
+    for filename in os.listdir(path_of_the_directory):
+        f1 = os.path.join(path_of_the_directory,filename)
+        if os.path.isfile(f1):
+            print(f1)
+            f2 = os.path.join(path_of_repo, filename)
+            if os.path.isfile(f2):
+                if getHash(f1) != getHash(f2):
+                    print("the file ", filename, "is not the same")
+
+    
+def getHash(filePath):
+    md5 = hashlib.md5()
+    with open(filePath,'rb') as file:
+        hash = file.read()
+        md5.update(hash)
+        return md5.hexdigest()
 
 if __name__ == "__main__":
     main()
