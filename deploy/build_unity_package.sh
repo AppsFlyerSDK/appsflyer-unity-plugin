e5ab941e64cbe06b3c87fabbd0a21c876348cf49
margot.guetta <margot.guetta@appsflyer.com>
2023-01-11 16:25:18 +0200

    test

--- deploy/build_unity_package.sh
+++ deploy/build_unity_package.sh
@@ -7,7 +7,7 @@ UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 PACKAGE_NAME="appsflyer-unity-plugin-6.6.0.unitypackage"
 mkdir -p $DEPLOY_PATH
 
-#add comment to test github action test
+#add comment to test github actions test
 # Build the .unitypackage
 /opt/Unity/Editor/Unity
 -gvh_disable \
