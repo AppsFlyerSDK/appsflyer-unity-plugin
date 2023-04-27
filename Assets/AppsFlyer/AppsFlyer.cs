5cf37a2520f8574d933970b8331c19d257426ca1
dmitry ovchinikov <dmitry.o@appsflyer.com>
2023-04-27 11:31:51 +0300

    version bump

--- Assets/AppsFlyer/AppsFlyer.cs
+++ Assets/AppsFlyer/AppsFlyer.cs
@@ -7,7 +7,7 @@ namespace AppsFlyerSDK
     public class AppsFlyer : MonoBehaviour
     {
 
-        public static readonly string kAppsFlyerPluginVersion = "6.10.20";
+        public static readonly string kAppsFlyerPluginVersion = "6.10.30";
         public static string CallBackObjectName = null;
         private static EventHandler onRequestResponse;
         private static EventHandler onInAppResponse;
