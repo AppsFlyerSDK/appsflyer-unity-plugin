8252488b8ad2778dce500ce4f2a690a8a9e14dba
af-do <101554260+af-do@users.noreply.github.com>
2023-08-27 15:29:05 +0300

    Merge pull request #243 from AppsFlyerSDK/releases/6.x.x/6.12.x/6.12.21-rc1
    Releases/6.x.x/6.12.x/6.12.21 rc1

---
+++ IAppsFlyerNativeBridge.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AppsFlyerSDK
+{
+
+    public interface IAppsFlyerNativeBridge
+    {
+        bool isInit { get; set; }
+
+        void startSDK(bool onRequestResponse, string CallBackObjectName);
+
+        void sendEvent(string eventName, Dictionary<string, string> eventValues, bool onInAppResponse, string CallBackObjectName);
+
+        void stopSDK(bool isSDKStopped);
+
+        bool isSDKStopped();
+
+        string getSdkVersion();
+
+        void setCustomerUserId(string id);
+
+        void setAppInviteOneLinkID(string oneLinkId);
+
+        void setAdditionalData(Dictionary<string, string> customData);
+
+        void setResolveDeepLinkURLs(params string[] urls);
+
+        void setOneLinkCustomDomain(params string[] domains);
+
+        void setCurrencyCode(string currencyCode);
+
+        void recordLocation(double latitude, double longitude);
+
+        void anonymizeUser(bool shouldAnonymizeUser);
+
+        string getAppsFlyerId();
+
+        void setMinTimeBetweenSessions(int seconds);
+
+        void setHost(string hostPrefixName, string hostName);
+
+        void setPhoneNumber(string phoneNumber);
+
+        void setSharingFilterForAllPartners();
+
+        void setSharingFilter(params string[] partners);
+
+        void getConversionData(string objectName);
+
+        void attributeAndOpenStore(string appID, string campaign, Dictionary<string, string> userParams, MonoBehaviour gameObject);
+
+        void recordCrossPromoteImpression(string appID, string campaign, Dictionary<string, string> parameters);
+
+        void generateUserInviteLink(Dictionary<string, string> parameters, MonoBehaviour gameObject);
+
+        void addPushNotificationDeepLinkPath(params string[] paths);
+
+        void setUserEmails(EmailCryptType cryptType, params string[] userEmails);
+
+        void subscribeForDeepLink(string objectName);
+
+        void setIsDebug(bool shouldEnable);
+
+        void setPartnerData(string partnerId, Dictionary<string, string> partnerInfo);
+    }
+}
---
+++ Plugins/iOS/AppsFlyeriOSWrapper.h
@@ -0,0 +1,47 @@
+//
+//  AppsFlyeriOSWarpper.h
+//  Unity-iPhone
+//
+//  Created by Jonathan Wesfield on 24/07/2019.
+//
+
+#import "AFUnityUtils.mm"
+#import "UnityAppController.h"
+#import "AppsFlyerAttribution.h"
+#if __has_include(<AppsFlyerLib/AppsFlyerLib.h>)
+#import <AppsFlyerLib/AppsFlyerLib.h>
+#else
+#import "AppsFlyerLib.h"
+#endif
+
+@interface AppsFlyeriOSWarpper : NSObject <AppsFlyerLibDelegate, AppsFlyerDeepLinkDelegate>
++ (BOOL) didCallStart;
++ (void) setDidCallStart:(BOOL)val;
+@end
+
+
+static AppsFlyeriOSWarpper *_AppsFlyerdelegate;
+static const int kPushNotificationSize = 32;
+
+static NSString* ConversionDataCallbackObject = @"AppsFlyerObject";
+
+static const char* VALIDATE_CALLBACK = "didFinishValidateReceipt";
+static const char* VALIDATE_ERROR_CALLBACK = "didFinishValidateReceiptWithError";
+static const char* GCD_CALLBACK = "onConversionDataSuccess";
+static const char* GCD_ERROR_CALLBACK = "onConversionDataFail";
+static const char* OAOA_CALLBACK = "onAppOpenAttribution";
+static const char* OAOA_ERROR_CALLBACK = "onAppOpenAttributionFailure";
+static const char* GENERATE_LINK_CALLBACK = "onInviteLinkGenerated";
+static const char* OPEN_STORE_LINK_CALLBACK = "onOpenStoreLinkGenerated";
+static const char* START_REQUEST_CALLBACK = "requestResponseReceived";
+static const char* IN_APP_RESPONSE_CALLBACK = "inAppResponseReceived";
+static const char* ON_DEEPLINKING = "onDeepLinking";
+
+
+static NSString* validateObjectName = @"";
+static NSString* openStoreObjectName = @"";
+static NSString* generateInviteObjectName = @"";
+static NSString* startRequestObjectName = @"";
+static NSString* inAppRequestObjectName = @"";
+static NSString* onDeeplinkingObjectName = @"";
+
