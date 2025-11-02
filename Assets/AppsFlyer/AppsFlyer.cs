ff9390087d2a9b329e9ca9f96dc62c0b9fa17495
veronica.b <veronica.b@appsflyer.com>
2025-11-02 17:33:54 +0200

    6.17.71

--- Assets/AppsFlyer/AppsFlyer.cs
+++ Assets/AppsFlyer/AppsFlyer.cs
@@ -6,7 +6,7 @@ namespace AppsFlyerSDK
 {
     public class AppsFlyer : MonoBehaviour
     {
-        public static readonly string kAppsFlyerPluginVersion = "6.17.7";
+        public static readonly string kAppsFlyerPluginVersion = "6.17.71";
         public static string CallBackObjectName = null;
         private static EventHandler onRequestResponse;
         private static EventHandler onInAppResponse;
