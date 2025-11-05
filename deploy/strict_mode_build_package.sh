ad318b6bea0420bbd704726a27b31939a42aed36
veronica.b <veronica.b@appsflyer.com>
2025-11-05 11:33:14 +0200

    6.17.71

--- deploy/strict_mode_build_package.sh
+++ deploy/strict_mode_build_package.sh
@@ -5,7 +5,7 @@ echo "Start Build for appsflyer-unity-plugin.unitypackage. Strict Mode."
 
  DEPLOY_PATH=outputs
  UNITY_PATH="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
- PACKAGE_NAME="appsflyer-unity-plugin-strict-mode-6.17.7.unitypackage"
+ PACKAGE_NAME="appsflyer-unity-plugin-strict-mode-6.17.71.unitypackage"
  mkdir -p $DEPLOY_PATH
 
 #move external dependency manager
@@ -41,7 +41,7 @@ echo "Commenting out functions. Done."
  Assets \
  $PWD/$DEPLOY_PATH/$PACKAGE_NAME \
  -quit \
- && echo "package exported successfully to outputs/appsflyer-unity-plugin-strict-mode-6.17.7.unitypackage" \
+ && echo "package exported successfully to outputs/appsflyer-unity-plugin-strict-mode-6.17.71.unitypackage" \
  || echo "Failed to export package. See create_unity_core.log for more info."
 
 
