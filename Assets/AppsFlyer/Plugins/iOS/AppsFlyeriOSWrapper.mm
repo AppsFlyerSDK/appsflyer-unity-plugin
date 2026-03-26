88474d27f39f914a26cb36aea4daba040f0bd8b8
Andrii H. <andrii.h@appsflyer.com>
2026-03-26 13:27:06 +0200

    AppsFlyerPluginVersion = "6.17.90"

--- Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm
+++ Assets/AppsFlyer/Plugins/iOS/AppsFlyeriOSWrapper.mm
@@ -33,7 +33,7 @@ extern "C" {
  
     const void _startSDK(bool shouldCallback, const char* objectName) {
         [[AppsFlyerLib shared] setPluginInfoWith: AFSDKPluginUnity
-                                pluginVersion:@"6.17.81"
+                                pluginVersion:@"6.17.90"
                                 additionalParams:nil];
         startRequestObjectName = stringFromChar(objectName);
         AppsFlyeriOSWarpper.didCallStart = YES;
